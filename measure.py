bf2941c5c10ffe3f4876228e9113a431398d52cc
houxiang <nudt_houxiang@163.com>
2016-01-03 20:57:51 +0800

    modify the file

--- measure.py
+++ measure.py
@@ -1,6 +1,8 @@
 import os
 import subprocess
 import logging
+import csv
+import re
 
 logging.basicConfig(level=logging.DEBUG,
                 format='%(asctime)s %(filename)s[line:%(lineno)d] %(levelname)s %(message)s',
@@ -10,10 +12,15 @@ logging.basicConfig(level=logging.DEBUG,
 
 logging.debug("this is the message !")
 
-log_indexs=[]
-email_list=[]
-contributor_efforts={}
 
+#put commits logs
+log_indexs=[]
+#put the commitors logs
+email_list=[]
+#put the lines of code made by contributors
+contributor_efforts={}
+#commits need tobe scaned
+commits_need_scaned=[]
 
 def commit_logs():
     cmd_log = ["git",
@@ -47,14 +54,14 @@ def get_indexs():
         if line != " ":
             strs = line.split(" ")
             log_indexs.append(strs[0])
+    log_indexs.reverse
     return log_indexs
 
 def run():
     get_indexs()
     length = len(log_indexs)
     print "the count of all commits is" + str(len(log_indexs))
-    log_indexs.reverse()
-    for index,item in list(enumerate(log_indexs)):
+    for index,item in list(enumerate(commits_need_scaned)):
         print str(index) +"the limit is "+str(length)
         flag = reset_history(item)
         if flag == 0:
@@ -64,19 +71,72 @@ def run():
 def commit_effort_count():
     #git log --pretty='%aN' | sort | uniq -c | sort -k1 -n -r
     state  = os.popen("git log --pretty='%aE' | sort | uniq -c | sort -k1 -n -r").read()
-
-    print state
+    writer=csv.writer(file('code_effort.csv','wb'))
+    writer.writerow(['contributor','commit_count','add','removed','total'])
     tmp_arr = state.split("\n")
     for line in tmp_arr:
+        csv_line=[]
         line = line.strip()
         arr = line.split(" ")
         if len(arr) == 2:
             email = line.split(" ")[1]
+            csv_line.append(email)
             commit_count = line.split(" ")[0]
-            print os.popen("git log --author="+email+" --pretty=tformat: --numstat | awk '{ add += $1 ; subs += $2 ; loc += $1 - $2 } END { printf \"added lines: \%s removed lines : \%s total lines: \%s\\n\",add,subs,loc }' - ").read()
+            csv_line.append(commit_count)
+            code_efforts = os.popen("git log --author="+email+" --pretty=tformat: --numstat | awk '{ add += $1 ; subs += $2 ; loc += $1 - $2 } END { printf \"added lines: \%s removed lines : \%s total lines: \%s\\n\",add,subs,loc }' - ").read()
+            add_remove_total = re.findall(r"\d+\.?\d*",code_efforts)
+            print add_remove_total
+            for num in add_remove_total:
+                csv_line.append(num)
+            writer.writerow(csv_line)
 
 
+def get_need_scan_commits():
+    get_indexs()
+    commits_and_changed_files = {}
+    #get index to do the git command
+    for line in log_indexs:
+        state = os.popen("git show "+line+" --stat").read()
+       # print state
+        tmp_arr =[]
+        for item in state.split("\n"):
+            if "|" in item:
+                change_files = item.split("|")[0].strip()
+                logging.info(change_files)
+                tmp_arr.append(change_files)
+        commits_and_changed_files[line] = tmp_arr
+    pre_change_files=[]
+
+    for index,log in list(enumerate(log_indexs)):
+        if index == len(log_indexs)-1:
+            commits_need_scaned.append(log)
+        elif index == 0:
+            pre_change_files.append(commits_and_changed_files[log])
+            pre_commit = log
+            continue
+        change_files = commits_and_changed_files[log]
+        if interlist(change_files,pre_change_files)==[]:
+            pre_change_files.append(change_files)
+            pre_commit = log
+        else:
+            commits_need_scaned.append(pre_commit)
+            pre_commit = log
+            pre_change_files=change_files
+    print commits_need_scaned
+    print len(commits_need_scaned)
+    print len(log_indexs)
+
+def interlist(a,b):
+    ret=[]
+    for i in a:
+        if i not in b:
+            ret.append(i)
+    return ret
 
 if __name__ == '__main__':
+    #first get the commits which we need to measure
+    get_need_scan_commits()
+    #reset to one version and measure
     run()
+    #get the code efforts of every contributor
     commit_effort_count()
